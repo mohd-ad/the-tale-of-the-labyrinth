5c38a5dac35fbcad867502a8730d1efb2b064ab7
Mohd <mohammad.eydi1420@gmail.com>
2025-12-08 09:10:11 +0330

    #2 Create the graphical user interface and organize functions.c

--- functions.c
+++ functions.c
@@ -6,153 +6,97 @@
 #include <ctype.h>
 #include <stdio.h>
 
-int initializeGameMap(int gameMap[12][12], int m, int n, int verticalWalls[12][2], int horizontalWalls[12][2])
-{
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
+#define MAX_SIZE 20
+
+
+int initializeGameMap(char gameMap[MAX_SIZE][MAX_SIZE], int m, int n,
+                      int verticalWalls[MAX_SIZE][MAX_SIZE], int horizontalWalls[MAX_SIZE][MAX_SIZE]) {
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
             gameMap[i][j] = '0';
         }
     }
-
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            printf("%c", gameMap[i][j]);
-        }
-        printf("\n");
-    }
     int coreLightX;
     int coreLightY;
-    int isget = 0;
 
-    while (isget == 0)
-    {
+    while (1) {
         scanf(" %d %d", &coreLightX, &coreLightY);
-        if (coreLightX < 0 || coreLightY < 0 || coreLightX >= m || coreLightY >= n)
-        {
+        if (coreLightX < 0 || coreLightY < 0 || coreLightX >= m || coreLightY >= n) {
             printf("Error: Out of Index [1]\n");
-            scanf(" %d %d", &coreLightX, &coreLightY);
-        }
-        else if (gameMap[coreLightX][coreLightY] == '0')
-        {
+        } else if (gameMap[coreLightX][coreLightY] == '0') {
             gameMap[coreLightX][coreLightY] = 'H';
-            isget = 1;
-        }
-        else
-        {
+            break;
+        } else {
             printf("Error: This is filled [1]\n");
         }
     }
 
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            printf("%c", gameMap[i][j]);
-        }
-        printf("\n");
-    }
     int surveyorCount;
-    int x, y;
-    isget = 0;
     scanf("%d", &surveyorCount);
     int surveyorLocations[surveyorCount][2];
-    for (int i = 0; i < surveyorCount || isget == 0; i++)
-    {
-        scanf("%d %d", &x, &y);
-        if (x >= m || y >= n || x < 0 || y < 0)
-        {
-            printf("Error: Out of Index [2]\n");
+
+    for (int i = 0; i < surveyorCount; i++) {
+        int x, y;
+        while (1) {
+            scanf("%d %d", &x, &y);
+            if (x >= m || y >= n || x < 0 || y < 0) {
+                printf("Error: Out of Index [2]\n");
+            } else if (gameMap[x][y] == '0') {
+                gameMap[x][y] = 'P';
+                surveyorLocations[i][0] = x;
+                surveyorLocations[i][1] = y;
+                break;
+            } else {
+                printf("Error: This is filled [2]\n");
+            }
         }
-        else if (gameMap[x][y] == '0')
-        {
-            gameMap[x][y] = 'P';
-            surveyorLocations[i][0] = x;
-            surveyorLocations[i][1] = y;
-            isget = 1;
-        }
-        else
-        {
-            printf("Error: This is filled [2]\n");
-        }
-    }
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            printf("%c", gameMap[i][j]);
-        }
-        printf("\n");
     }
     int shadyCount;
 
-    int r, c;
-    isget = 0;
     scanf("%d", &shadyCount);
     int shadyLocations[shadyCount][2];
-    for (int i = 0; i < shadyCount || isget == 0; i++)
-    {
-        scanf("%d %d", &r, &c);
-        if (r >= m || c >= n || r < 0 || c < 0)
-        {
-            printf("Error: Out of Index [3]\n");
-        }
-        else if (gameMap[r][c] == '0')
-        {
-            gameMap[r][c] = 'S';
-            shadyLocations[i][0] = r;
-            shadyLocations[i][1] = c;
-            isget = 1;
-        }
-        else
-        {
-            printf("Error: This is filled [3]\n");
+    for (int i = 0; i < shadyCount; i++) {
+        int r, c;
+        while (1) {
+            scanf("%d %d", &r, &c);
+            if (r >= m || c >= n || r < 0 || c < 0) {
+                printf("Error: Out of Index [3]\n");
+            } else if (gameMap[r][c] == '0') {
+                gameMap[r][c] = 'S';
+                shadyLocations[i][0] = r;
+                shadyLocations[i][1] = c;
+                break;
+            } else {
+                printf("Error: This is filled [3]\n");
+            }
         }
     }
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            printf("%c", gameMap[i][j]);
-        }
-        printf("\n");
-    }
+
     int countWall;
 
     scanf("%d", &countWall);
 
-    for (int i = 0; i < countWall; i++)
-    {
+    for (int i = 0; i < countWall; i++) {
         int x, y;
         char jahat;
         scanf("%d %d %c", &x, &y, &jahat);
 
-        if (toupper(jahat) == 'H')
-        {
-            if (x >= m || y >= n - 1 || x < 0 || y < 0)
-            {
+        if (toupper(jahat) == 'H') {
+            if (x >= m || y >= n - 1 || x < 0 || y < 0) {
                 printf("Error: Out of Index [4]\n");
                 i--;
                 continue;
             }
             horizontalWalls[x][y] = 1;
-        }
-        else if (toupper(jahat) == 'V')
-        {
-            if (x >= m - 1 || y >= n || x < 0 || y < 0)
-            {
+        } else if (toupper(jahat) == 'V') {
+            if (x >= m - 1 || y >= n || x < 0 || y < 0) {
                 printf("Error: Out of Index [4]\n");
                 i--;
 
                 continue;
             }
             verticalWalls[x][y] = 1;
-        }
-        else
-        {
+        } else {
             printf("Error: This is filled [4]\n");
             i--;
         }
@@ -161,47 +105,3 @@ int initializeGameMap(int gameMap[12][12], int m, int n, int verticalWalls[12][2
     return 0;
 }
 
-
-int printGameMap(char gameMap[12][12], int m, int n, int verticalWalls[12][2], int horizontalWalls[12][2])
-{
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            printf("%c", gameMap[i][j]);
-            if (verticalWalls[i][j] == 1)
-            {
-                printf("|");
-            }
-            else
-            {
-                printf(" ");
-            }
-        }
-        printf("\n");
-        for (int j = 0; j < n - 1; j++)
-        {
-            if (horizontalWalls[i][j] == 1)
-            {
-                printf("_");
-            }
-            else printf(" ");
-        }
-        printf("\n");
-    }
-    return 0;
-}
-
-// int main()
-// {
-//     int gameMap[12][12];
-//     int gameMapWidth, gameMapHeight;
-//     int verticalWalls[gameMapWidth-1][gameMapHeight];
-//     int horizontalWalls[gameMapWidth][gameMapHeight-1];
-//     scanf("%d %d", &gameMapWidth, &gameMapHeight);
-//
-//     initializeGameMap(gameMap, gameMapWidth, gameMapHeight, verticalWalls, horizontalWalls);
-//
-//
-//     printGameMap(gameMap, gameMapWidth, gameMapHeight, verticalWalls, horizontalWalls);
-// }
--- main.c
+++ main.c
@@ -1,26 +1,57 @@
-#include "raylib.h"
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "raylib.h"
+#include "functions.c"
 
-int main(void)
-{
 
+#define MAX_SIZE 20
+#define BLOCK_SIZE 60
+
+
+int main(void) {
     // Initialization
     //--------------------------------------------------------------------------------------
-    int screenWidth = 800;
-    int screenHeight = 450;
+    int screenWidth = 1000;
+    int screenHeight = 800;
+    int itemSize = BLOCK_SIZE / 2 - 1;
+    int offset = BLOCK_SIZE / 2 + 1;
 
-    scanf("%d", &screenWidth);
-    scanf("%d", &screenHeight);
+    int m, n;
+    scanf("%d", &m);
+    scanf("%d", &n);
+    if (m > MAX_SIZE || n > MAX_SIZE || m <= 3 || n <= 3) {
+        printf("Error: Grid size must be between 3 and %d\n", MAX_SIZE);
+        return -1;
+    }
+
+    char gameMap[MAX_SIZE][MAX_SIZE];
+    // Verticals are m-1, n
+    int verticalWalls[MAX_SIZE][MAX_SIZE];
+    // Horizontals are m, n-1
+    int horizontalWalls[MAX_SIZE][MAX_SIZE];
+
+    for (int i = 0; i < MAX_SIZE; i++) {
+        for (int j = 0; j < MAX_SIZE; j++) {
+            gameMap[i][j] = '0';
+            verticalWalls[i][j] = 0;
+            horizontalWalls[i][j] = 0;
+        }
+    }
+
+    initializeGameMap(gameMap, m, n, verticalWalls, horizontalWalls);
+
 
     InitWindow(screenWidth, screenHeight, "TTOTL");
 
     // TODO: Load resources / Initialize variables at this point
+    Texture2D background = LoadTexture("assets/background.jpg");
 
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
 
     // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose()) // Detect window close button or ESC key
     {
         // Update
         //----------------------------------------------------------------------------------
@@ -31,21 +62,62 @@ int main(void)
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-        ClearBackground(RED);
+        ClearBackground(RAYWHITE);
+        DrawTexture(background, 0, 0, WHITE);
+        // 40 * m = width
+        // 40 * n = hegiht
+        int aNumber = (screenWidth - BLOCK_SIZE * m) / 2;
+        int firstPointUp[2] = {aNumber, (screenHeight - BLOCK_SIZE * n) / 2};
 
-        // TODO: Draw everything that requires to be drawn at this point
+        DrawRectangle(firstPointUp[0], firstPointUp[1], BLOCK_SIZE * m + 13, BLOCK_SIZE * n + 13, BLACK);
+        DrawRectangle(firstPointUp[0] + 5, firstPointUp[1] + 5, BLOCK_SIZE * m + 2, BLOCK_SIZE * n + 2, GRAY);
 
-        DrawLineEx((Vector2){ 0, 0 }, (Vector2){ screenWidth, screenHeight }, 2.0f, RED);
-        DrawLineEx((Vector2){ 0, screenHeight }, (Vector2){ screenWidth, 0 }, 2.0f, RED);
-        DrawText("example base code template", 260, 400, 20, LIGHTGRAY);
 
-        DrawText("This is Black!", screenWidth/3, screenHeight/2, 40, BLACK);
+        int startMapV[2] = {firstPointUp[0] + 5, firstPointUp[1] + 5};
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                DrawRectangle(startMapV[0] + (BLOCK_SIZE * i) + 2, startMapV[1] + (BLOCK_SIZE * j) + 2,
+                              itemSize * 2 - 1, itemSize * 2 - 1, WHITE);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m-1; j++) {
+                if (verticalWalls[i][j] == 1) {
+                    DrawRectangle(startMapV[0] + (BLOCK_SIZE * (j+1))-1, startMapV[1] + (BLOCK_SIZE * i) + 1, 4,
+                                  itemSize * 2 + 2, BLACK);
+                }
+            }
+        }
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < m; j++) {
+                if (horizontalWalls[i][j] == 1) {
+                    DrawRectangle(startMapV[0] + (BLOCK_SIZE * j), startMapV[1] + (BLOCK_SIZE * (i+1)) -1, itemSize * 2 + 2,
+                                  4, BLACK);
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                char now = toupper(gameMap[i][j]);
+                if (now == 'H') {
+                    DrawRectangle(startMapV[0] + (BLOCK_SIZE * j) + 2, startMapV[1] + (BLOCK_SIZE * i) + 2,
+                                  itemSize * 2 - 1, itemSize * 2 - 1, YELLOW);
+                } else if (now == 'P') {
+                    DrawCircle(startMapV[0] + (BLOCK_SIZE * j) + offset-1,
+                               startMapV[1] + (BLOCK_SIZE * i) + BLOCK_SIZE / 2, itemSize, RED);
+                } else if (now == 'S') {
+                    DrawCircle(startMapV[0] + (BLOCK_SIZE * j) + offset-1,
+                               startMapV[1] + (BLOCK_SIZE * i) + BLOCK_SIZE / 2, itemSize, BLUE);
+                }
+            }
+        }
+
+
+
         EndDrawing();
 
-
-        if (IsKeyDown(KEY_UP))
-        {
-
+        if (IsKeyDown(KEY_UP)) {
         }
         //----------------------------------------------------------------------------------
     }
@@ -53,9 +125,9 @@ int main(void)
     // De-Initialization
     //--------------------------------------------------------------------------------------
 
-    // TODO: Unload all loaded resources at this point
+    UnloadTexture(background);
 
-    CloseWindow();        // Close window and OpenGL context
+    CloseWindow(); // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
     return 0;
